6a5ae081107ada2288a3f6a5d229849cf510a791
Scott Ginebaugh <spginebaugh@gmail.com>
2024-03-27 20:17:16 -0700

    added libraries

---
+++ utils/rna_doublet_scoring_functions.R
@@ -0,0 +1,15 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Load Libraries                              ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(Seurat)
+library(qs)
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(SingleCellExperiment)
+
+library(scds)
+library(scater)
+library(DoubletFinder)
+library(scDblFinder)
+
