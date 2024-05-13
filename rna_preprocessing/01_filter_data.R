225b18a0f50c9ea204c84cb2b9b16456e6aee5f5
Scott Ginebaugh <spginebaugh@gmail.com>
2024-05-12 17:42:15 -0700

    combined all metadata info

--- rna_preprocessing/01_filter_data.R
+++ rna_preprocessing/01_filter_data.R
@@ -25,21 +25,45 @@ seurat <- import_seurat(
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## import
 meta_sample <- read_csv("manuscript_metadata/manuscript_table_S1_sample_info.csv")
-meta_cell_rna <- read_csv("manuscript_metadata/manuscript_table_S3_scrna_meta.csv")
 meta_doublet <- read_csv("data/processed_data/doublet_metadata.csv")
+meta_cell_rna <- read_csv("manuscript_metadata/manuscript_table_S3_scrna_meta.csv")
+
 
 ## organize
 seurat$sample_ID <- word(seurat$sample,2,-1,"_")
+seurat$barcode_ID <- paste0(seurat$sample_ID, "_", word(seurat$barcode,-1,-1,"_"))
+
 colnames(meta_sample)[1] <- "sample_ID"
 
-seurat$barcode_ID
 colnames(meta_cell_rna)[1] <- "barcode_ID"
 meta_cell_rna$barcode_ID <- paste0(meta_cell_rna$Sample, "_", word(meta_cell_rna$barcode_ID,-1,-1,"_"))
+meta_cell_rna <- meta_cell_rna[,c(1,9,10,11)]
+colnames(meta_cell_rna) <- c(
+  "barcode_ID",
+  "ms_broad_ct", # manuscript broad celltype
+  "ms_fine_ct",
+  "ms_veryfine_ct"
+  )
 
 ## combine
 metadata <- seurat@meta.data
+
 metadata <- left_join(
   metadata, 
   meta_sample[,c("sample_ID","rounded_age","sex","preservation","disease_status")],
   by = "sample_ID"
-  )
+)
+
+metadata <- left_join(
+  metadata,
+  meta_doublet[,c("barcode", "scDbl_class", "cxds_score", "bcds_score",
+                  "hybrid_score", "DF_score", "DF_classification")],
+  by = "barcode"
+)
+
+metadata <- left_join(
+  metadata,
+  meta_cell_rna,
+  by = "barcode_ID"
+)
+
