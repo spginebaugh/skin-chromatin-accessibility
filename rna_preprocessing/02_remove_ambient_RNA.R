b9bb784278dcb556cbd09788ab03710f69932179
Scott Ginebaugh <spginebaugh@gmail.com>
2024-06-04 16:46:16 -0700

    added doublet removal

--- rna_preprocessing/02_remove_ambient_RNA.R
+++ rna_preprocessing/02_remove_ambient_RNA.R
@@ -4,6 +4,7 @@
 library(Seurat)
 library(qs)
 library(scCDC)
+library(harmony)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                 Import Data                               ----
@@ -12,36 +13,49 @@ output_seurat_file <- "data/processed_data/ambient_removed_seurat.qs"
 
 seurat <- qread("data/processed_data/filtered_seurat.qs")
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                preprocessing                             ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# filter out low expression genes
+seurat <- seurat[rowSums(GetAssayData(seurat, slot = "counts", assay = "RNA") > 1) > 10,]
+
 decontam <- seurat
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                            run decontamination                           ----
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 decontam <- decontam %>% NormalizeData()
 decontam <- decontam %>% FindVariableFeatures()
 decontam <- decontam %>% ScaleData()
 decontam <- decontam %>% RunPCA()
 
-decontam <- decontam %>% RunUMAP(reduction = "pca", dims = 1:40)
-
-## view umap to decide if batch correction is necessary
-DimPlot(decontam, group.by = c("sample", "age_group"))
-
 ## batch correct
 decontam <- decontam %>% RunHarmony(group.by.vars = "sample", dims.use = 1:40, assay.use = "RNA_ranger")
 decontam <- decontam %>% RunUMAP(reduction = "harmony", dims = 1:40)
-DimPlot(decontam, group.by = c("sample", "age_group"))
+DimPlot(decontam, group.by = c("sample", "sex","disease_status"))
+DimPlot(decontam, group.by = c("ms_broad_ct", "ms_fine_ct"))
+DimPlot(decontam, group.by = c("scDbl_class", "DF_classification"))
 
 ## cluster
 decontam <- decontam %>% FindNeighbors(reduction = "harmony", dims = 1:40)
-decontam <- decontam %>% FindClusters(resolution = c(0.2, 0.4))
+decontam <- decontam %>% FindClusters(resolution = c(0.6))
 
 ## select resolution
-DimPlot(decontam, group.by = c("RNA_ranger_snn_res.0.2", "RNA_ranger_snn_res.0.4"))
+DimPlot(decontam, group.by = c("RNA_snn_res.0.6"), label = TRUE)
+
+## remove doublets 
+## based on separation of doublet clusters, 
+## we are going to be aggressive in doublet removal
+decontam <- decontam[,!(decontam$RNA_snn_res.0.6 %in% c(14,24))]
+decontam <- decontam[, (decontam$scDbl_class == "singlet") & (decontam$DF_classification == "Singlet")]
+
+## recluster for input to scCDC
+
 
 ## select snn_res.0.4 as resolution
 Idents(decontam) <- decontam$RNA_ranger_snn_res.0.4
 
 decontam <- decontam %>% ScaleData(features = rownames(decontam))
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                            run decontamination                           ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 gcgs <- ContaminationDetection(decontam)
