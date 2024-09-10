bc5d9552c2327f6c58bbb448e39f24e76fd46a48
Scott Ginebaugh <spginebaugh@gmail.com>
2024-09-09 17:28:55 -0700

    added doublet detection

---
+++ atac_preprocessing/01_signac_doublets_with_amulet.R
@@ -0,0 +1,42 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Load Libraries                              ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(Seurat)
+library(qs)
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(SingleCellExperiment)
+library(tidyverse)
+library(harmony)
+
+library(Signac)
+library(GenomicRanges)
+library(scDblFinder)
+
+library(ggrastr)
+
+library(AnnotationHub)
+library(BiocParallel)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                Add Doublets                              ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+frag_path <- "data/atac_GSE212448/fragments/"
+frag_files <- list.files("data/atac_GSE212448/fragments/")
+frag_files <- frag_files[!(frag_files %in% grep("tbi",frag_files, value = TRUE))]
+frag_file_names <- word(frag_files,1,1,"\\.")
+frag_file_names <- word(frag_file_names,1,-2,"_")
+
+repeats <- GRanges("chr6", IRanges(1000,2000))
+otherChroms <- GRanges(c("M","chrM","MT","X","Y","chrX","chrY"),IRanges(1L,width=10^8))
+toExclude <- suppressWarnings(c(repeats, otherChroms))
+
+amulet_doublets <- list()
+for (i in 1:length(frag_files)){
+  amulet_doublets[[i]] <- amulet(paste0(frag_path,frag_files[i]),
+                            regionsToExclude=toExclude,
+                            fullInMemory = TRUE,
+                            BPPARAM = mcparam)
+}
+qsave(amulet_doublets,"data/processed_data/amulet_doublets.qs")
