ff2a1aaf2b659a27264e34908c1c252971cb4559
Scott Ginebaugh <spginebaugh@gmail.com>
2024-09-11 14:30:12 -0700

    added parallel processing for doublet detection

--- atac_preprocessing/01_signac_doublets_with_amulet.R
+++ atac_preprocessing/01_signac_doublets_with_amulet.R
@@ -18,13 +18,10 @@ library(ggrastr)
 
 library(AnnotationHub)
 library(BiocParallel)
-
+library(parallel)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                Add Doublets                              ----
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-options(MulticoreParam=MulticoreParam(workers=20))
-mcparam <- MulticoreParam(workers = 20)
-
 frag_path <- "data/atac_GSE212448/fragments/"
 frag_files <- list.files("data/atac_GSE212448/fragments/")
 frag_files <- frag_files[!(frag_files %in% grep("tbi",frag_files, value = TRUE))]
@@ -35,11 +32,15 @@ repeats <- GRanges("chr6", IRanges(1000,2000))
 otherChroms <- GRanges(c("M","chrM","MT","X","Y","chrX","chrY"),IRanges(1L,width=10^8))
 toExclude <- suppressWarnings(c(repeats, otherChroms))
 
-amulet_doublets <- list()
-for (i in 1:length(frag_files)){
-  amulet_doublets[[i]] <- amulet(paste0(frag_path,frag_files[i]),
-                            regionsToExclude=toExclude,
-                            fullInMemory = TRUE,
-                            BPPARAM = mcparam)
-}
+amulet_doublets <- mclapply(frag_files, function(x) {
+  amulet_out <- amulet(paste0(frag_path,x),
+                           regionsToExclude=toExclude,
+                           fullInMemory = TRUE,
+                           BPPARAM = NULL)
+                                                       
+  return(amulet_out)
+}, mc.cores = getOption("mc.cores", 6))
+names(amulet_doublets) <- frag_file_names
+
+
 qsave(amulet_doublets,"data/processed_data/amulet_doublets.qs")
