2ef7bf8b219634463147787a2ec3e5a79e412a40
Scott Ginebaugh <spginebaugh@gmail.com>
2024-09-12 17:12:02 -0700

    Added Demultiplexing

--- atac_preprocessing/02_ATAC_filtering.R
+++ atac_preprocessing/02_ATAC_filtering.R
@@ -30,8 +30,44 @@ meta_cell_atac <- read_csv("manuscript_metadata/manuscript_table_S2_scatac_meta.
 
 
 
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                  Organize                                ----
+#                                Demultiplex                               ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+add_demuxlet_results <- function(signac_obj_meta, best_file, sample_names){
+  best_dat <- readr::read_tsv(best_file) %>% data.frame()
+  best_class <- stringr::str_split(best_dat$BEST, pattern = "-", simplify = TRUE)[,1]
+  cell_names <- paste0(sample_names,"_", best_dat$BARCODE)
+  idx <- which(cell_names %in% rownames(signac_obj_meta))
+  
+  signac_obj_meta$DemuxletClassify <- "NotClassified"
+  signac_obj_meta$DemuxletBest <- "NotClassified"
+  
+  signac_obj_meta[cell_names[idx], "DemuxletClassify"] <- ifelse(
+    stringr::str_split(best_dat$BEST, pattern = "-", simplify=TRUE)[idx,1] %in% c("AMB","DBL"),
+    stringr::str_split(best_dat$BEST, pattern = "-", simplify=TRUE)[idx,1],
+    stringr::str_split(best_dat$BEST, pattern = "-", simplify=TRUE)[idx,2]
+  )
+  signac_obj_meta[cell_names[idx], "DemuxletBest"] <- gsub("AMB-","",gsub("DBL-","",gsub("SNG-","",best_dat$BEST)))[idx]
+  return(signac_obj_meta)
+}
+
+seurat@meta.data <- add_demuxlet_results(seurat@meta.data, "manuscript_outputs/C_SD_POOL.best", "GSE212448_C_SD_POOL")
+
+demuxConvert <- c(
+  "C_SD_01_S15" = "C_SD4",
+  "C_SD_06_S16" = "C_SD5",
+  "C_SD_08_S17" = "C_SD6",
+  "C_SD_10_S18" = "C_SD7"
+)
+
+seurat$sample <- ifelse(seurat$DemuxletBest == "NotClassified", seurat$sample_ID, demuxConvert[seurat$DemuxletBest])
+seurat <- seurat[,!(seurat$DemuxletClassify %in% c("AMB","DBL")) & !(seurat$sample %in% "C_SD_POOL")]
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Organize Metadata                           ----
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 for (i in names(dblt)){
   dblt[[i]]$sample <- i
