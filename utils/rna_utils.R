fed20fcbfb2d8d1b8a74e400d03a42a3dd765ba3
Scott Ginebaugh <spginebaugh@gmail.com>
2024-09-14 14:16:52 -0700

    added score_panglao function

--- utils/rna_utils.R
+++ utils/rna_utils.R
@@ -47,7 +47,30 @@ import_seurat <- function(cellranger_folder_path, file_names_vec, file_h5_path =
 
 
 
-
+score_panglao <- function(seurat_obj){
+  # returns a matrix of scaled celltype scores for each cluster 
+  panglao <- readr::read_tsv("data/processed/PanglaoDB_markers_27_Mar_2020.tsv")
+  panglao <- panglao[panglao$species %in% c("Hs", "Mm Hs"), ]
+  panglao <- panglao[panglao$`official gene symbol` %in% rownames(seurat_obj), ]
+  panglao <- split(panglao$`official gene symbol`, panglao$`cell type`)
+  
+  seurat_obj <- AddModuleScore(seurat_obj, panglao)
+  
+  metadata <- seurat_obj@meta.data
+  
+  metadata_name_start <- (ncol(metadata) - length(panglao) + 1)
+  metadata_name_end <- ncol(metadata)
+  
+  pang_ann <- metadata[,metadata_name_start:metadata_name_end] 
+  colnames(pang_ann)<- names(panglao)
+  
+  pang_ann <- scale(pang_ann)
+  agg_pang <- aggregate(pang_ann, list(seurat_obj@active.ident), mean)
+  rownames(agg_pang) <- agg_pang[, 1]
+  agg_pang <- agg_pang[, -1]
+  agg_pang <- t(agg_pang)
+  return(agg_pang)
+}
 
 
 
