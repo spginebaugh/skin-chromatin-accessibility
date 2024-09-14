335fabe771a9042aec7f3de355da4f37dd9ea5d8
Scott Ginebaugh <spginebaugh@gmail.com>
2024-09-14 14:16:28 -0700

    Added QC, filtering, and clustering

--- atac_preprocessing/00_ATAC_preprocessing_signac.R
+++ atac_preprocessing/00_ATAC_preprocessing_signac.R
@@ -12,12 +12,12 @@ library(harmony)
 
 library(Signac)
 library(GenomicRanges)
+library(scDblFinder)
 
 library(ggrastr)
 
-library(BSgenome.Hsapiens.UCSC.hg38)
 library(AnnotationHub)
-
+library(BiocParallel)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                 Import Data                               ----
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -44,25 +44,6 @@ for (i in 1:length(frag_files)){
 qsave(seurat_list, "data/processed_data/signac_list.qs")
 
 
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# #                                Add Doublets                              ----
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# mcparam <-(MulticoreParam(workers = multicoreWorkers()))
-# 
-# repeats <- GRanges("chr6", IRanges(1000,2000))
-# otherChroms <- GRanges(c("M","chrM","MT","X","Y","chrX","chrY"),IRanges(1L,width=10^8))
-# toExclude <- suppressWarnings(c(repeats, otherChroms))
-# 
-# amulet_doublets <- list()
-# for (i in 1:length(frag_files)){
-#   amulet_doublets[[i]] <- amulet(paste0(frag_path,frag_files[i]), 
-#                             regionsToExclude=toExclude,
-#                             fullInMemory = TRUE,
-#                             BPPARAM = mcparam)
-# }
-# qsave(amulet_doublets,"data/processed_data/amulet_doublets.qs")
-
-
 seurat <- merge(
   x = seurat_list[[1]],
   y = seurat_list[2:length(seurat_list)],
@@ -71,174 +52,6 @@ seurat <- merge(
 colnames(seurat@meta.data)[1] <- "sample_ID" ## since we need to demuxlet some of the samples
 seurat$barcode <- colnames(seurat)
 qsave(seurat,"data/processed_data/signac_merge.qs")
-
-# run_scDblFinder_atac <- function(seurat_split){
-#   for (sample_index in 1:length(seurat_split)) {
-#     dbl_out <- scDblFinder(
-#       GetAssayData(seurat_split[[sample_index]], assay = "ATAC", slot = "counts"),
-#       returnType = "table",
-#       aggregateFeatures = TRUE,
-#       nfeatures=25
-#     ) %>% as.data.frame()
-#     dbl_out$barcode <- rownames(dbl_out)
-#     dbl_out <- dbl_out[, c("barcode", "class")]
-#     colnames(dbl_out) <- c("barcode", "scDbl_class")
-#     
-#     ## add scoring into seurat metadata
-#     metadata <- seurat_split[[sample_index]]@meta.data
-#     metadata <- left_join(metadata, dbl_out, by = "barcode")
-#     rownames(metadata) <- metadata$barcode
-#     seurat_split[[sample_index]]@meta.data <- metadata
-#   }
-#   return(seurat_split)
-# }
-# 
-# seurat_list <- run_scDblFinder_atac(seurat_list)
-
-
-##################
-ccd <- getCellColData(atac_proj)
-ccd[ , "DemuxletClassify"] <- "NotClassified"
-ccd[ , "DemuxletBest"] <- "NotClassified"
-best <- data.frame(readr::read_tsv("manuscript_outputs/C_SD_POOL.best"))
-classification <- stringr::str_split(best$BEST, pattern = "-", simplify=TRUE)[,1]
-cellNames <- paste0("GSE212448_C_SD_POOL", "#", best$BARCODE)
-idx <- which(cellNames %in% rownames(ccd))
-ccd[ cellNames[idx], "DemuxletClassify"] <- ifelse(
-  stringr::str_split(best$BEST, pattern = "-", simplify=TRUE)[idx,1] %in% c("AMB","DBL"),
-  stringr::str_split(best$BEST, pattern = "-", simplify=TRUE)[idx,1],
-  stringr::str_split(best$BEST, pattern = "-", simplify=TRUE)[idx,2]
-)
-ccd[ cellNames[idx], "DemuxletBest"] <- gsub("AMB-","",gsub("DBL-","",gsub("SNG-","",best$BEST)))[idx]
-##################
-
-
-add_demuxlet_results <- function(signac_obj_meta, best_file, sample_names){
-  best_dat <- readr::read_tsv(best_file) %>% data.frame()
-  best_class <- stringr::str_split(best_dat$BEST, pattern = "-", simplify = TRUE)[,1]
-  idx <- which(best_dat$BARCODE %in% rownames(signac_obj_meta))
-  
-  signac_obj_meta$DemuxletClassify <- "NotClassified"
-  signac_obj_meta$DemuxletBest <- "NotClassified"
-  
-  signac_obj_meta[rownames(signac_obj_meta)[idx], "DemuxletClassify"] <- ifelse(
-    stringr::str_split(best_dat$BEST, pattern = "-", simplify=TRUE)[idx,1] %in% c("AMB","DBL"),
-    stringr::str_split(best_dat$BEST, pattern = "-", simplify=TRUE)[idx,1],
-    stringr::str_split(best_dat$BEST, pattern = "-", simplify=TRUE)[idx,2]
-  )
-  signac_obj_meta[rownames(signac_obj_meta)[idx], "DemuxletBest"] <- gsub("AMB-","",gsub("DBL-","",gsub("SNG-","",best_dat$BEST)))[idx]
-  return(signac_obj_meta)
-}
-
-pool_meta <- add_demuxlet_results(seurat_list[["GSE212448_C_SD_POOL"]]@meta.data, "manuscript_outputs/C_SD_POOL.best", "GSE212448_C_SD_POOL")
-
-demuxConvert <- c(
-  "C_SD_01_S15" = "C_SD4",
-  "C_SD_06_S16" = "C_SD5",
-  "C_SD_08_S17" = "C_SD6",
-  "C_SD_10_S18" = "C_SD7"
-)
-
-pool_meta$sample <- ifelse(pool_meta$DemuxletBest == "NotClassified", pool_meta$orig.ident, demuxConvert[pool_meta$DemuxletBest])
-  
-seurat <- merge(
-  x = seurat_list[[1]],
-  y = seurat_list[2:length(seurat_list)],
-  add.cell.id = frag_file_names
-)
-colnames(seurat@meta.data)[1] <- "sample"
-seurat$barcode <- colnames(seurat)
-
-qsave(seurat, "data/processed_data/signac_merged.qs")
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                Add Metadata                               ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                               Add Annotation                              ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ah <- AnnotationHub()
-query(ah, "EnsDb.Hsapiens.v98")
-ensdb_v98 <- ah[["AH75011"]]
-annotations <- GetGRangesFromEnsDb(ensdb = ensdb_v98)
-seqlevels(annotations) <- paste0('chr', seqlevels(annotations))
-genome(annotations) <- "hg38"
-Annotation(seurat) <- annotations
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                   QC                                      ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-seurat <- NucleosomeSignal(object = seurat)
-seurat <- TSSEnrichment(object = seurat)
-
-## dont have necessary metadata for this step
-# seurat$pct_reads_in_peaks <- seurat$peak_region_fragments / seurat$passed_filters * 100
-
-seurat$blacklist_ratio <- FractionCountsInRegion(
-  object = seurat, 
-  assay = 'ATAC',
-  regions = blacklist_hg38_unified
-)
-
-qsave(seurat, "data/processed_data/signac_merged2.qs")
-
-
-DensityScatter(seurat, x = 'nCount_ATAC', y = 'TSS.enrichment', log_x = TRUE, quantiles = TRUE)
-seurat$nucleosome_group <- ifelse(seurat$nucleosome_signal > 4, 'NS > 4', 'NS < 4')
-FragmentHistogram(object = seurat, group.by = 'nucleosome_group')
-VlnPlot(
-  object = seurat,
-  features = c('nCount_ATAC', 'TSS.enrichment', 'blacklist_ratio', 'nucleosome_signal'),
-  pt.size = 0.1,
-  ncol = 5
-)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                                  Filtering                               ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-seurat <- subset(
-  x = seurat,
-  subset = nCount_peaks > 9000 &
-    nCount_peaks < 100000 &
-    pct_reads_in_peaks > 40 &
-    blacklist_ratio < 0.01 &
-    nucleosome_signal < 4 &
-    TSS.enrichment > 4
-)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                             Normalization                                ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-seurat <- RunTFIDF(seurat)
-seurat <- FindTopFeatures(seurat, min.cutoff = 'q0')
-seurat <- RunSVD(seurat)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                              Clustering                                  ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-DepthCor(seurat)
-
-seurat <- RunUMAP(object = seurat, reduction = 'lsi', dims = 2:30)
-seurat <- FindNeighbors(object = seurat, reduction = 'lsi', dims = 2:30)
-seurat <- FindClusters(object = seurat, verbose = FALSE, algorithm = 3)
-DimPlot(object = seurat, label = TRUE) + NoLegend()
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                              Gene Activity                               ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-gene.activities <- GeneActivity(seurat)
-seurat[['ATAC_RNA']] <- CreateAssayObject(counts = gene.activities)
-seurat <- NormalizeData(
-  object = seurat,
-  assay = 'ATAC_RNA',
-  normalization.method = 'LogNormalize',
-  scale.factor = median(seurat$nCount_RNA)
-)
-DefaultAssay(seurat) <- 'ATAC_RNA'
-
-qsave(seurat, "data/processed_data/signac_clustered.qs")
 
 
 
--- atac_preprocessing/02_ATAC_filtering.R
+++ atac_preprocessing/02_ATAC_filtering.R
@@ -18,6 +18,8 @@ library(ggrastr)
 library(AnnotationHub)
 library(BiocParallel)
 
+library(harmony)
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                 Import Data                               ----
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -66,7 +68,7 @@ demuxConvert <- c(
 seurat$sample <- ifelse(seurat$DemuxletBest == "NotClassified", seurat$sample_ID, demuxConvert[seurat$DemuxletBest])
 seurat <- seurat[,!(seurat$DemuxletClassify %in% c("AMB","DBL")) & !(seurat$sample %in% "GSE212448_C_SD_POOL")]
 
-seurat$sample_name <- str_remove_all(seurat$sample, "^GS.*?_")
+seurat$sample <- str_remove_all(seurat$sample, "^GS.*?_")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                              Organize Metadata                           ----
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -77,7 +79,7 @@ dblt <- do.call(rbind, dblt)
 rownames(dblt) <- dblt$barcode
 colnames(dblt)[1:6] <- paste0("amulet_", colnames(dblt)[1:6])
 
-colnames(meta_sample)[1] <- "sample_name"
+colnames(meta_sample)[1] <- "sample"
 
 seurat$metaname_barcode <- word(seurat$barcode,2,-1,"_")
 colnames(meta_cell_atac) <- paste0("manuscript_", colnames(meta_cell_atac))
@@ -86,8 +88,136 @@ meta_cell_atac$metaname_barcode <- str_replace_all(meta_cell_atac$metaname_barco
 
 metadata <- seurat@meta.data
 metadata <- left_join(metadata, dblt, by = "barcode")
-metadata <- left_join(metadata, meta_sample, by = "sample_name")
+metadata <- left_join(metadata, meta_sample, by = "sample")
 metadata <- left_join(metadata, meta_cell_atac, by = "metaname_barcode")
 
 rownames(metadata) <- metadata$barcode
 seurat@meta.data <- metadata
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                               Add Annotation                              ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ah <- AnnotationHub()
+query(ah, "EnsDb.Hsapiens.v98")
+ensdb_v98 <- ah[["AH75011"]]
+annotations <- GetGRangesFromEnsDb(ensdb = ensdb_v98)
+seqlevels(annotations) <- paste0('chr', seqlevels(annotations))
+genome(annotations) <- "hg38"
+Annotation(seurat) <- annotations
+
+
+
+peaks_keep <- seqnames(granges(seurat)) %in% standardChromosomes(granges(seurat))
+seurat <- pbmc[as.vector(peaks_keep), ]
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                   QC                                      ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+seurat <- NucleosomeSignal(object = seurat)
+seurat <- TSSEnrichment(object = seurat)
+seurat$blacklist_ratio <- FractionCountsInRegion(
+  object = seurat, 
+  assay = 'ATAC',
+  regions = blacklist_hg38_unified
+)
+
+qsave(seurat, "data/processed_data/signac_merged2.qs")
+
+
+
+DensityScatter(seurat, x = 'nCount_ATAC', y = 'TSS.enrichment', log_x = TRUE, quantiles = TRUE)
+seurat$nucleosome_group <- ifelse(seurat$nucleosome_signal > 4, 'NS > 4', 'NS < 4')
+
+FragmentHistogram(object = seurat, group.by = 'nucleosome_group')
+VlnPlot(
+  object = seurat,
+  features = c('nCount_ATAC', 'TSS.enrichment', 'blacklist_ratio', 'nucleosome_signal'),
+  pt.size = 0.1,
+  ncol = 5
+)
+
+metadata <- seurat@meta.data
+metadata %>%
+  ggplot(aes(color = sample, x = nCount_ATAC, fill = sample)) +
+  geom_density(alpha = 0.2) +
+  scale_x_log10() +
+  theme_classic() +
+  ylab("Cell density") +
+  geom_vline(xintercept = 400) +
+  geom_vline(xintercept = 20000)
+
+metadata %>%
+  ggplot(aes(color = sample, x = TSS.enrichment, fill = sample)) +
+  geom_density(alpha = 0.2) +
+  scale_x_log10() +
+  theme_classic() +
+  ylab("Cell density") +
+  geom_vline(xintercept = 3) 
+
+metadata %>%
+  ggplot(aes(color = sample, x = blacklist_ratio, fill = sample)) +
+  geom_density(alpha = 0.2) +
+  scale_x_log10() +
+  theme_classic() +
+  ylab("Cell density") +
+  geom_vline(xintercept = 0.01) 
+
+metadata %>%
+  ggplot(aes(color = sample, x = nucleosome_signal, fill = sample)) +
+  geom_density(alpha = 0.2) +
+  scale_x_log10() +
+  theme_classic() +
+  ylab("Cell density") +
+  geom_vline(xintercept = 3) 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                  Filtering                               ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+seurat <- subset(
+  x = seurat,
+  subset = nCount_ATAC > 400 &
+    nCount_ATAC < 20000 &
+    blacklist_ratio < 0.01 &
+    nucleosome_signal < 3 &
+    TSS.enrichment > 3 &
+    amulet_q.value > 0.01 # remove doublets
+)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                             Normalization                                ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+seurat <- RunTFIDF(seurat)
+seurat <- FindTopFeatures(seurat, min.cutoff = 'q0')
+seurat <- RunSVD(seurat)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Clustering                                  ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DepthCor(seurat)
+#' first component has strongly negative correlation,
+#' so we exclude from downstream processing
+
+seurat <- RunHarmony(seurat, group.by.vars = "sample", reduction.use = 'lsi', dims.use = 2:30, assay.use = "ATAC", project.dim = FALSE)
+seurat <- RunUMAP(object = seurat, reduction = 'harmony', dims = 1:29)
+seurat <- FindNeighbors(object = seurat, reduction = 'lsi', dims = 1:29)
+seurat <- FindClusters(object = seurat, 
+                       algorithm = 3, 
+                       resolution = c(0.6,0.8,1))
+DimPlot(object = seurat, label = TRUE) + NoLegend()
+DimPlot(object = seurat, group.by = c("sample","manuscript_BroadClust"), label = TRUE) + NoLegend()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Gene Activity                               ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+gene.activities <- GeneActivity(seurat)
+seurat[['ATAC_RNA']] <- CreateAssayObject(counts = gene.activities)
+seurat <- NormalizeData(
+  object = seurat,
+  assay = 'ATAC_RNA',
+  normalization.method = 'LogNormalize',
+  scale.factor = median(seurat$nCount_RNA)
+)
+DefaultAssay(seurat) <- 'ATAC_RNA'
+
+qsave(seurat, "data/processed_data/signac_clustered.qs")
+
+
