2c477c55405fa988ca960fbd7a960e04bb19276f
Scott Ginebaugh <spginebaugh@gmail.com>
2024-10-28 16:30:48 -0700

    Added Motif information. Still need to fix co-assessable network analysis.

---
+++ atac_preprocessing/03_add_additional_information.R
@@ -0,0 +1,91 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                              Load Libraries                              ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(Seurat)
+library(qs)
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(SingleCellExperiment)
+library(tidyverse)
+library(harmony)
+
+library(Signac)
+library(GenomicRanges)
+
+library(ggrastr)
+
+library(AnnotationHub)
+library(BiocParallel)
+
+library(harmony)
+
+
+library(JASPAR2020)
+library(TFBSTools)
+library(BSgenome.Hsapiens.UCSC.hg38)
+library(patchwork)
+
+library(monocle3)
+library(cicero)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                 Import Data                               ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+seurat <- qread("data/processed_data/signac_clustered.qs")
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                 Motif Info                               ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DefaultAssay(seurat) <- "ATAC"
+seqinfo(seurat) <- seqinfo(Annotation(seurat))
+
+pfm <- getMatrixSet(
+  x = JASPAR2020,
+  opts = list(collection = "CORE", tax_group = 'vertebrates', species = "Homo sapiens", all_versions = FALSE)  
+)
+
+seurat <- AddMotifs(
+  object = seurat,
+  genome = BSgenome.Hsapiens.UCSC.hg38,
+  assay = "ATAC",
+  pfm = pfm
+)
+
+seurat <- RegionStats(
+  object = seurat,
+  genome = BSgenome.Hsapiens.UCSC.hg38,
+  assay = "ATAC"
+)
+
+seurat <- RunChromVAR(
+  object = seurat,
+  genome = BSgenome.Hsapiens.UCSC.hg38,
+  assay = "ATAC"
+)
+
+qsave(seurat, "data/processed_data/signac_additional_info.qs")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                               Co-accessible network                     ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+cds <- as.CellDataSet(x = seurat)
+cicero <- make_cicero_cds(cds, reduced_coordinates = reducedDimS(cds))
+
+
+genome <- Signac::seqlengths(seurat)
+genome_df <- data.frame("chr" = names(genome), "length" = genome)
+conns <- run_cicero(cds, genomic_coords = genome.df, sample_num = 100)
+
+ccans <- generate_ccans(conns)
+
+links <- ConnectionsToLinks(conns = conns, ccans = ccans)
+
+## add into seurat
+Links(seurat) <- links
+
+qsave(seurat, "data/processed_data/")
